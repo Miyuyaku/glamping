a4d5b2bb63e644602341ab94660c10a6139f843f
Miyu <miyunoasara@gmail.com>
2022-09-01 13:17:33 +0900

    9.2

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
+<html lang="ja">
 <head>
-    <html lang="ja">
     <title>GRANPING</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +9,6 @@
     <!-- css -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/footer.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/home.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/home.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/archive-blog.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/sanitize.css">
